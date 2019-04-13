ac096f4174018c9fcb404071a5e170736969ff8d
Harsh Lathwal <xeon.harsh@gmail.com>
2019-04-13 12:13:33 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import (Flask, jsonify, make_response, redirect, render_template, request, session)
+from flask_cors import CORS
+from uuid import uuid4
+import datetime
+
+
+@app.route('/api/api', methods=['POST'])
+def some_api():
+    input = request.form.get("input")
+    if not input:
+        return jsonify({"status": 0, "message": "no input"})
+    return jsonify(call a function)
+
+app.route('/', defaults={'path': ''})
+
+
+@app.route('/<path:path>')
+def init(path):
+    return render_template('index.html')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)

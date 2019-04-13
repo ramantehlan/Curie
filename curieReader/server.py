c2e35225923246fd6f81a657a84ff3a311cee74a
raman tehlan <ramantehlan@gmail.com>
2019-04-14 05:15:12 +0530

    Minor changes

--- curieReader/server.py
+++ curieReader/server.py
@@ -8,7 +8,7 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route("/",methods=["get"])
 def get():
     print("inside API")
-    with open("./curieReader/data.txt","r") as file:
+    with open("./curieReader/heart_data.txt","r") as file:
         data = file.read()
     return jsonify(json.loads(data))
 

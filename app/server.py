dd7dc67f68a7d7301a186c55274a26e34621e2ea
raman tehlan <ramantehlan@gmail.com>
2019-04-13 23:14:14 +0530

    Minor changes

--- app/server.py
+++ app/server.py
@@ -1,19 +1,8 @@
-from flask import Flas
+from flask import *
 import json
 
 app = Flask(__name__)
 app.route('/')
-
-@app.route("/get/v1",methods=["get"])
-def grt_v1():
-    print('in api')
-    s = socket.socket()
-    port = 6969
-    s.connect(('10.1.2.233',port))
-    print('connected')
-    da = json.loads(s.recv(1024).decode('ascii'))
-    #s.close()
-    return json.dumps(da)
 
 @app.route('/')
 def home():

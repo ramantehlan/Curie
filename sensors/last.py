8787ebc0571e68e7c91df386ea58fdc77a877826
raman tehlan <ramantehlan@gmail.com>
2019-04-14 06:09:35 +0530

    Add the ecg data

--- sensors/last.py
+++ sensors/last.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import os
 
 # The second argument is the baudrate, change according to the baudrate you gave to your Serial.begin command
 ser = serial.Serial("/dev/ttyACM0",9600)
@@ -10,4 +11,11 @@ time.sleep(1);
 ser.setDTR(False)
 
 while True:
+    lawda='ecgdata.txt'
     print ser.readline()
+    file = open(lawda, 'w') 
+    file.write(ser.readline()) 
+    file.close() 
+ 
+
+
